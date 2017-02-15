a0179896292ec798483f1ac9c3a0b777d852a759
yangnan2009 <ynsina@vip.sina.com>
2017-02-15 10:52:07 +0800

    This is example code for learning Angular2.

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { AuthService } from './core/auth.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path:'login',
+        component: LoginComponent
+      }
+    ])
+  ],
+  providers: [
+    {provide: 'auth',  useClass: AuthService}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: `
+    <div layout="row" layout-align="center center">
+      <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
+        <fieldset ngModelGroup="login">
+          <label>用户名：</label>
+          <input type="text"
+            name="username"
+            [(ngModel)]="username"
+            #usernameRef="ngModel"
+            required
+            minlength="3"
+            placeholder="请输入用户名"
+            />
+            <div *ngIf="usernameRef.errors?.minlength">should be at least 3 charactors</div>
+          <label>用户口令：</label>
+          <input type="password"
+            name="password"
+            [(ngModel)]="password"
+            #passwordRef="ngModel"
+            required
+            placeholder="请输入用户密码"
+            />
+          <button type="submit">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  `,
+  styles: [`
+    input.ng-invalid{
+      border: 3px solid red;
+    }
+    input.ng-valid{
+      border: 3px solid green;
+    }
+  `],
+  providers: []
+})
+export class LoginComponent implements OnInit {
+  username = "";
+  password = "";
+
+  constructor(@Inject('auth') private service) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(formValue){
+    console.log('auth result is: '
+      + this.service.loginWithCredentials(formValue.login.username, formValue.login.password));
+  }
+}
+
+
