8453530d79247158f66b904135e5ce47d672613f
yangnan2009 <ynsina@vip.sina.com>
2017-02-16 16:15:00 +0800

    This is example code for learning Angular2.

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.model';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
+})
+export class TodoComponent implements OnInit {
+  todos : Todo[] = [];
+  desc: string = '';
+
+  constructor(private service: TodoService) {}
+  ngOnInit() {
+    this.getTodos();
+  }
+  addTodo(){
+    this.service
+      .addTodo(this.desc)
+      .then(todo => {
+        this.todos = [...this.todos, todo];
+        this.desc = '';
+      });
+  }
+  toggleTodo(todo: Todo) {
+    const i = this.todos.indexOf(todo);
+    this.service
+      .toggleTodo(todo)
+      .then(t => {
+        this.todos = [
+          ...this.todos.slice(0,i),
+          t,
+          ...this.todos.slice(i+1)
+        ];
+      });
+  }
+  removeTodo(todo: Todo) {
+    const i = this.todos.indexOf(todo);
+    this.service
+      .deleteTodoById(todo.id)
+      .then(()=> {
+        this.todos = [
+          ...this.todos.slice(0,i),
+          ...this.todos.slice(i+1)
+        ];
+      });
+  }
+  getTodos(): void {
+    this.service
+      .getTodos()
+      .then(todos => this.todos = [...todos]);
+  }
+}
