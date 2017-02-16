c6f874da3a16d7108bde1ef7d18c051f8d89853b
yangnan2009 <ynsina@vip.sina.com>
2017-02-16 16:12:28 +0800

    This is example code for learning Angular2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
 import { AuthService } from './core/auth.service';
+import { routing } from './app.routes';
+import { TodoComponent } from './todo/todo.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryTodoDbService } from './todo/todo-data';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-      },
-      {
-        path:'login',
-        component: LoginComponent
-      }
-    ])
+    InMemoryWebApiModule.forRoot(InMemoryTodoDbService),
+    routing
   ],
   providers: [
     {provide: 'auth',  useClass: AuthService}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by YangNan on 2017/2/15.
+ */
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TodoComponent } from './todo/todo.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todo',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+
+];
+
+export const routing = RouterModule.forRoot(routes);
